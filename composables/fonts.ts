411dc23a1b30527e3d072407d8cce669391ec2d9
Valeriy <valeriy.gnybidenko@gmail.com>
2022-07-31 15:30:52 +0300

    fix: delete not usable fonts: 10min

--- composables/fonts.ts
+++ composables/fonts.ts
@@ -2,14 +2,6 @@ const fontsMetaList = [
   {
     rel: 'preload',
     type: 'font/woff2',
-    href: '/fonts/ClashDisplay-Bold.woff2',
-    crossorigin: 'anonymous',
-    as: 'font',
-  },
-
-  {
-    rel: 'preload',
-    type: 'font/woff2',
     href: '/fonts/ClashDisplay-Light.woff2',
     crossorigin: 'anonymous',
     as: 'font',
@@ -34,22 +26,6 @@ const fontsMetaList = [
   {
     rel: 'preload',
     type: 'font/woff2',
-    href: '/fonts/SeriouslyNostalgic-Regular.woff2',
-    crossorigin: 'anonymous',
-    as: 'font',
-  },
-
-  {
-    rel: 'preload',
-    type: 'font/woff2',
-    href: '/fonts/SeriouslyNostalgicFn-Cond.woff2',
-    crossorigin: 'anonymous',
-    as: 'font',
-  },
-
-  {
-    rel: 'preload',
-    type: 'font/woff2',
     href: '/fonts/SeriouslyNostalgicFn-Regular.woff2',
     crossorigin: 'anonymous',
     as: 'font',
@@ -67,22 +43,6 @@ const fontsMetaList = [
     rel: 'preload',
     type: 'font/woff2',
     href: '/fonts/SeriouslyNostalgicFnIt-Reg.woff2',
-    crossorigin: 'anonymous',
-    as: 'font',
-  },
-
-  {
-    rel: 'preload',
-    type: 'font/woff2',
-    href: '/fonts/SeriouslyNostalgicItal-Cond.woff2',
-    crossorigin: 'anonymous',
-    as: 'font',
-  },
-
-  {
-    rel: 'preload',
-    type: 'font/woff2',
-    href: '/fonts/SeriouslyNostalgicItal-UCn.woff2',
     crossorigin: 'anonymous',
     as: 'font',
   },
