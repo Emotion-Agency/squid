8727f94113d1503725013c216db8f48346b06155
Valeriy <valeriy.gnybidenko@gmail.com>
2022-08-01 17:58:41 +0300

    feature: add home-3 and home-4 sections: 1h

---
+++ components/icons/ArrowRight.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    width="14"
+    height="26"
+    viewBox="0 0 14 26"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0.5 25.5L12.5 13.5L0.5 0.5" stroke="#777777" />
+  </svg>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ useTransition()
       <div class="container home-1__wrapper">
         <h1 class="home-1__title">
           JUMP
-          <span class="home-1__italic-title"> IN</span>
+          <span class="home-1__style-title"> IN</span>
         </h1>
         <NextPageButton>
           Don’t worry, the water’s fine
@@ -24,7 +24,7 @@ useTransition()
       <div class="container home-2__wrapper">
         <h1 class="home-2__title">
           DIVE
-          <span class="home-2__italic-title"> DEEP</span>
+          <span class="home-2__style-title"> DEEP</span>
         </h1>
         <NextPageButton>
           Into uncharted territory
@@ -35,5 +35,47 @@ useTransition()
       </div>
       <TheSocials />
     </section>
+    <div class="wrapper">
+      <section class="section section--nm home-3">
+        <div class="container home-3__wrapper">
+          <h1 class="home-3__title">
+            BENEATH
+            <span class="home-3__style-title home-3__style-title--1"> THE</span>
+            <span class="home-3__style-title home-3__style-title--2">
+              SURFACE</span
+            >
+          </h1>
+          <NextPageButton>
+            This is where we thrive
+            <span class="next-page-btn__arrow">
+              <IconsArrowDown class="next-page-btn__arrow-img" />
+            </span>
+          </NextPageButton>
+        </div>
+        <TheSocials />
+      </section>
+      <section class="section section--nm home-4">
+        <div class="container home-4__wrapper">
+          <h1 class="home-4__text">
+            The first step in
+            <span class="home-4__style-text">ELEVATING A BRAND</span> is jumping
+            in. It takes <span class="home-4__style-text">COURAGE</span> and
+            <span class="home-4__style-text">commitment</span>, but we don’t
+            embark alone. We
+            <span class="home-4__style-text">INVITE YOUR TEAM</span> to be an
+            integrated part of ours as we
+            <span class="home-4__style-text">NAVIGATE</span> deep into
+            <span class="home-4__style-text">UNCHARTED waters</span>.
+          </h1>
+          <NextPageButton>
+            Let’s explore
+            <span class="next-page-btn__arrow">
+              <IconsArrowDown class="next-page-btn__arrow-img" />
+            </span>
+          </NextPageButton>
+        </div>
+        <TheSocials />
+      </section>
+    </div>
   </main>
 </template>
