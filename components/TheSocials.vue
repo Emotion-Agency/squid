8b060ecada41a11a6e993698bbb0802f855a7445
Valeriy <valeriy.gnybidenko@gmail.com>
2022-08-02 21:57:44 +0300

    feature: responsive for header, bottom-nav and home-1 done: 30min

--- components/TheSocials.vue
+++ components/TheSocials.vue
@@ -1,12 +1,22 @@
 <template>
   <ul class="socials">
     <li class="socials__li">
-      <a href="#" class="socials__link" target="_blank" rel="noreferer noopener">
+      <a
+        href="#"
+        class="socials__link"
+        target="_blank"
+        rel="noreferer noopener"
+      >
         <IconsFacebook class="socials__socials-img" />
       </a>
     </li>
     <li class="socials__li">
-      <a href="#" class="socials__link" target="_blank" rel="noreferer noopener">
+      <a
+        href="#"
+        class="socials__link"
+        target="_blank"
+        rel="noreferer noopener"
+      >
         <IconsLinkedin class="socials__socials-img" />
       </a>
     </li>
