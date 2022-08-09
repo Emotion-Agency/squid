9eb68ae5cbc1eee8d1c1704920c00f9e8c176354
Valeriy <valeriy.gnybidenko@gmail.com>
2022-08-09 10:16:10 +0300

    feature: about-1 section complete: 30min

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useTransition } from '~/composables/transition'
+useTransition()
+</script>
+
+<template>
+  <main>
+    <section class="section section--nm about-1">
+      <div class="container about-1__wrapper">
+        <div class="about-1__text-wrapper">
+          <h1 class="about-1__title">
+            WE ARE THE AGENCY
+            <span class="about-1__style-title"> SQUID</span>.
+          </h1>
+        </div>
+      </div>
+      <!-- <BottomNavigation /> -->
+    </section>
+  </main>
+</template>
