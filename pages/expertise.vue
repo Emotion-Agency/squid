4a273758dc1d065e057105d8c2656742c50a195a
Valeriy <valeriy.gnybidenko@gmail.com>
2022-08-09 14:45:54 +0300

    feature: expertise-1 section complete: 20min

---
+++ pages/expertise.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useTransition } from '~/composables/transition'
+useTransition()
+</script>
+
+<template>
+  <main>
+    <section class="section section--nm expertise-1">
+      <div class="container expertise-1__wrapper">
+        <div class="expertise-1__text-wrapper">
+          <h1 class="expertise-1__title">
+            WE ARE
+            <span class="expertise-1__style-title"> EXPERT</span> COMMUNICATORS.
+          </h1>
+        </div>
+      </div>
+      <BottomNavigation class="navigation-notfixed" />
+    </section>
+  </main>
+</template>
