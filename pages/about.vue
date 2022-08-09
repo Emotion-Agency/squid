69eab5be3e8a2cb0007021aa19356f57bb901a13
Valeriy <valeriy.gnybidenko@gmail.com>
2022-08-09 11:03:14 +0300

    feature: about-2,3 complete:40min

--- pages/about.vue
+++ pages/about.vue
@@ -14,7 +14,25 @@ useTransition()
           </h1>
         </div>
       </div>
-      <!-- <BottomNavigation /> -->
+      <BottomNavigation class="navigation-notfixed" />
+    </section>
+    <section class="section about-2">
+      <div class="container about-2__wrapper">
+        <div class="about-2__text-wrapper">
+          <p class="about-2__text">
+            We are an industry-agnostic agency that focuses on doing great work
+            for great people. We think big. We think smart. We ask the tough
+            questions, and shed light in dark spaces. We are a driven team that
+            partners with driven teams. We are a passionate group of talented
+            individuals that are here to make your business better.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="section about-3">
+      <div class="container grid about-3__wrapper">
+        <TheImage class="about-3__img" src="/images/about/1.jpg" alt="Image" />
+      </div>
     </section>
   </main>
 </template>
