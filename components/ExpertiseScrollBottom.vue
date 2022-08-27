0c5d29d183eda16e985fb9dc6924783253876ff4
letmewin22 <lkosteckiy5@gmail.com>
2022-08-27 13:40:48 +0300

    feat: expertise scroll bottom: 0.5h

---
+++ components/ExpertiseScrollBottom.vue
@@ -0,0 +1,22 @@
+<script lang='ts' setup>
+const onClick = () => {
+  window.ss.state.target = window.ss.max
+}
+</script>
+
+<template>
+  <section class="section expertise-8">
+    <div class="container expertise-8__wrapper">
+      <button
+        class="expertise-8__btn"
+        @click="onClick"
+      >
+        <span class="expertise-8__text">Step One</span>
+        <span class="expertise-8__text">CONTACT US</span>
+        <span class="expertise-8__arrow">
+          <IconsArrowBtnDown />
+        </span>
+      </button>
+    </div>
+  </section>
+</template>
--- pages/expertise.vue
+++ pages/expertise.vue
@@ -122,16 +122,6 @@ onBeforeUnmount(() => {
         </div>
       </div>
     </section>
-    <section class="section expertise-8">
-      <div class="container expertise-8__wrapper">
-        <button class="expertise-8__btn">
-          <span class="expertise-8__text">Step One</span>
-          <span class="expertise-8__text">CONTACT US</span>
-          <span class="expertise-8__arrow">
-            <IconsArrowBtnDown />
-          </span>
-        </button>
-      </div>
-    </section>
+    <ExpertiseScrollBottom />
   </main>
 </template>
