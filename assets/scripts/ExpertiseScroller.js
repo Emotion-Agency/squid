c0d6790694ca5efd19e0dbf22f1dd358f43acab8
letmewin22 <lkosteckiy5@gmail.com>
2022-08-27 13:33:59 +0300

    feat: expertise scroller: 1h

---
+++ assets/scripts/ExpertiseScroller.js
@@ -0,0 +1,51 @@
+import gsap from 'gsap'
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export class ExpertiseScroller {
+  constructor($el, $wrapper, $children) {
+    this.$el = $el
+    this.$wrapper = $wrapper
+    this.$children = [...$children]
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    this.init()
+  }
+
+  init() {
+    if (screen.width > 1060) {
+      this.setHeight()
+      this.animate()
+    }
+  }
+
+  animate() {
+    this.tl = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: this.$el.querySelector('.container'),
+          pin: false,
+          scrub: 0.1,
+          start: 'top',
+          end: this.$el.getBoundingClientRect().bottom - window.innerHeight,
+          scroller: document.getElementById('scroll-container'),
+        },
+      })
+      .to(this.$wrapper, {
+        y:
+          -this.$wrapper.scrollHeight +
+          this.$children[this.$children.length - 1].scrollHeight,
+        ease: 'none',
+      })
+  }
+
+  setHeight() {
+    this.$el.style.height =
+      this.$children.length * (window.innerHeight * 0.5) + 'px'
+  }
+
+  destroy() {
+    this.tl && this.tl.destroy()
+  }
+}
--- components/ExpertiseScroller.vue
+++ components/ExpertiseScroller.vue
@@ -1,20 +1,50 @@
+<script lang='ts' setup>
+let es
+onMounted(async () => {
+  const { ExpertiseScroller } = await import('~/assets/scripts/ExpertiseScroller')
+  const $el = document.querySelector('.expertise-6')
+  const $wrapper = document.querySelector('.expertise-6__text-wrapper')
+  const $children = document.querySelectorAll('.expertise-6__text')
+
+  es = new ExpertiseScroller($el,$wrapper,$children)
+})
+
+onBeforeUnmount(() => {
+  es && es.destroy()
+})
+</script>
+
 <template>
   <section class="section expertise-6">
     <div class="container expertise-6__wrapper">
       <h2 class="expertise-6__title">ROI</h2>
-      <div class="expertise-6__text-wrapper">
-        <p class="expertise-6__text">
-          COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
-        </p>
-        <p class="expertise-6__text">
-          COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
-        </p>
-        <p class="expertise-6__text">
-          COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
-        </p>
-        <p class="expertise-6__text">
-          COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
-        </p>
+      <div class="expertise-6__scroll-wrapper">
+        <div class="expertise-6__text-wrapper">
+          <p class="expertise-6__text">
+            COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
+          </p>
+          <p class="expertise-6__text">
+            Id senectus cras magna cursus id aliquet sit fames dignissim.
+          </p>
+          <p class="expertise-6__text">
+            Morbi porttitor diam tristique sodales neque id nisl tempus.
+          </p>
+          <p class="expertise-6__text">
+            Feugiat in mauris porttitor consequat est tortor viverra.
+          </p>
+          <p class="expertise-6__text">
+            COSTA FARMS MET THEIR $1 BILLION FINANCIAL GOAL.
+          </p>
+          <p class="expertise-6__text">
+            Id senectus cras magna cursus id aliquet sit fames dignissim.
+          </p>
+          <p class="expertise-6__text">
+            Morbi porttitor diam tristique sodales neque id nisl tempus.
+          </p>
+          <p class="expertise-6__text">
+            Feugiat in mauris porttitor consequat est tortor viverra.
+          </p>
+        </div>
       </div>
     </div>
   </section>
