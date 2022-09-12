81df389f3fc620c9d744c8504d3d601aab159d30
letmewin22 <lkosteckiy5@gmail.com>
2022-09-12 12:06:32 +0300

    fix: update expertise scroller

--- assets/scripts/ExpertiseScroller.js
+++ assets/scripts/ExpertiseScroller.js
@@ -12,21 +12,26 @@ export class ExpertiseScroller extends Scrolling {
   }
 
   resize() {
-    this.setHeight()
+    // this.setHeight()
     if (screen.width >= 1060) {
       super.init()
     }
   }
 
+  get length() {
+    return this.$children.length
+  }
+
   get top() {
     const min = 0
-    const max = 892
+    const max =
+      this.$el.getBoundingClientRect().height -
+      this.$children[this.length - 1].getBoundingClientRect().height
     return this.computeFromMinToMax(min, max)
   }
 
   setHeight() {
-    this.$el.style.height =
-      this.$children.length * (window.innerHeight * 0.5) + 'px'
+    this.$el.style.height = this.length * (window.innerHeight * 0.5) + 'px'
   }
 
   onScroll() {
