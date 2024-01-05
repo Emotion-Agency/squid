942d909e5a0af284245004e7a68b4c1b7a9c73e2
letmewin22 <lkosteckiy5@gmail.com>
2024-01-05 15:36:23 +0200

    feat: update form

--- components/TheForm.vue
+++ components/TheForm.vue
@@ -36,23 +36,41 @@ const inputsList = reactive([
     error: true,
     value: '',
   },
+  {
+    title: 'Message',
+    required: false,
+    id: 'message',
+    name: 'Message',
+    placeholder: 'Type Field',
+    type: 'textarea',
+    error: false,
+    value: '',
+  },
 ])
+
+const isErrors = computed(
+  () => !!inputsList.filter(input => input.error).length
+)
 
 const $inputs = ref(null)
 
 const onSubmit = async () => {
   try {
+    const formData = new FormData()
+
+    inputsList.forEach(el => {
+      formData.append(el.name, el.value)
+    })
     await fetch('https://formspree.io/f/xrgngrzb', {
       method: 'POST',
-      body: JSON.stringify({
-        name: inputsList[0].value,
-        email: inputsList[1].value,
-        subject: inputsList[2].value,
-        message: inputsList[3].value,
-      }),
+      body: formData,
+      mode: 'no-cors',
     })
-    inputsList.forEach(input => (input.value = ''))
-    $inputs.value?.forEach(input => input?.reset())
+    setTimeout(() => {
+      inputsList.forEach(input => (input.value = ''))
+      $inputs.value?.forEach(input => input?.reset())
+    }, 0)
+    alert('Thank you for your message!')
   } catch (error) {
     alert('Something went wrong! Pease try again later.')
     console.log(error)
@@ -60,7 +78,10 @@ const onSubmit = async () => {
 }
 
 const onChange = val => {
-  console.log(val)
+  const input = inputsList.find(input => input.id === val.id)
+  if (!input) return
+  input.value = val.value
+  input.error = val.error
 }
 </script>
 
@@ -82,16 +103,8 @@ const onChange = val => {
         :placeholder="input.placeholder"
         @input-value="onChange"
       />
-      <TheInput
-        id="message"
-        title="Message"
-        name="Message"
-        type="textarea"
-        placeholder="Type Field"
-        @input-value="onChange"
-      />
     </div>
-    <button type="submit" class="form__btn">
+    <button type="submit" class="form__btn" :disabled="isErrors">
       <span>SEND</span>
     </button>
   </form>
