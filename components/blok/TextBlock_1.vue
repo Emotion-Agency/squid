ab70c71ba5a7d9286844719a3fe26657ff9302a9
letmewin22 <lkosteckiy5@gmail.com>
2024-03-01 17:07:01 +0200

    fix: text block 1 bug

--- components/blok/TextBlock_1.vue
+++ components/blok/TextBlock_1.vue
@@ -14,7 +14,11 @@ interface iProps {
 const props = defineProps<iProps>()
 
 const typeOfMainText = computed(() => {
-  return typeof props.blok.main_text
+  return typeof props.blok?.main_text
+})
+
+const typeOfTitle = computed(() => {
+  return typeof props.blok?.title
 })
 
 const breakLine = useBreakLine()
@@ -24,16 +28,25 @@ const breakLine = useBreakLine()
   <div class="container">
     <div class="text-block-1" :class="!blok.aligned && 'text-block-1--aligned'">
       <RichText
-        v-if="blok.title"
+        v-if="
+          typeOfTitle === 'object' &&
+          Array.isArray(blok.title) &&
+          blok?.title?.length
+        "
         class="text-block-1__title"
         :text="blok.title[0].text"
       />
+      <RichText
+        v-if="blok?.title && typeOfTitle === 'string'"
+        class="text-block-1__title"
+        :text="blok.title"
+      />
       <p
-        v-if="blok.main_text && typeOfMainText === 'string'"
+        v-if="blok?.main_text && typeOfMainText === 'string'"
         v-html="breakLine(blok.main_text)"
       />
       <div
-        v-else-if="blok.main_text && typeOfMainText === 'object'"
+        v-else-if="blok?.main_text && typeOfMainText === 'object'"
         class="text-block-1__desc"
       >
         <RichText :text="blok.main_text" />
